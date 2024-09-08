687c7c50b931549174b2af7c7f9a80e263c8e1de
Kartik Maheshwari <95483381+Kartik-Maheshwari@users.noreply.github.com>
2024-09-08 22:22:33 +0530

    Handled forntend

---
+++ src/components/UserInfoCard.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const UserInfoCard = ({ userId }: { userId: string }) => {
+  return (
+    <div className="p-4 bg-white rounded-lg shadow-md text-sm flex flex-col gap-4">
+      <div className="flex justify-between items-center font-medium">
+        <span className="text-gray-500">User Information</span>
+        <Link href="/" className="text-blue-500 text-xs">
+          See all
+        </Link>
+      </div>
+      {/* BOTTOM */}
+      <div className="flex flex-col gap-4 text-gray-500">
+        <div className="flex items-center gap-2">
+          <span className="text-xl text-black">Lloyd Fleming</span>
+          <span className="text-sm">@john</span>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+          velit ex expedita libero necessitatibus laudantium a asperiores ullam
+          beatae fuga, in nisi. Dicta a maxime unde, aspernatur atque debitis
+          fugit?
+        </p>
+
+        <div className="flex items-center gap-2">
+          <Image src="/map.png" alt="" width={16} height={16} />
+          <span>
+            Living in <b>Denver</b>
+          </span>
+        </div>
+        <div className="flex items-center gap-2">
+          <Image src="/school.png" alt="" width={16} height={16} />
+          <span>
+            Went to <b>school</b>
+          </span>
+        </div>
+
+        <div className="flex items-center gap-2">
+          <Image src="/work.png" alt="" width={16} height={16} />
+          <span>
+            Works at <b>Apple Inc.</b>
+          </span>
+        </div>
+        <div className="flex items-center justify-between">
+          <div className="flex gap-1 items-center">
+            <Image src="/link.png" alt="" width={16} height={16} />
+            <Link href="gttps://lama.dev" className="text-blue-500 font-medium">
+              lama.dev
+            </Link>
+          </div>
+          <div className="flex gap-1 items-center">
+            <Image src="/date.png" alt="" width={16} height={16} />
+            <span>Joined November 2024</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserInfoCard;
